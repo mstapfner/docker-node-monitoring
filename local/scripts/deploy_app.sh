1708529d7ae72ff250f87c4d7e95e87cd92efe74
ansjin <anshuljindal.nith@gmail.com>
2018-12-20 12:22:48 +0100

    update

---
+++ local/scripts/deploy_app.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+apt-get remove -y docker docker-engine docker.io
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce
+service docker start
+FILE="/etc/docker/daemon.json"
+/bin/cat <<EOM >$FILE
+{
+  "metrics-addr" : "127.0.0.1:9323",
+  "experimental" : true
+}
+EOM
+sudo service docker restart
+curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+cd ..
+sudo chmod 777 ./grafana/setup.sh
+sudo docker-compose up --build &
+read -p "Press enter to Exit"
